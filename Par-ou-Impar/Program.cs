fa65fd448b88f2ee872a8e0f51edc37467709191
Andersson-Sandoval <andersson.sval@hotmail.com>
2022-06-19 16:48:42 -0300

    Adicionar arquivos de projeto.

---
+++ Par-ou-Impar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Par_ou_Impar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com um número inteiro: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Par!");
+            }
+
+            else { Console.WriteLine("Impar!"); }
+        }
+            
+    }
+}
